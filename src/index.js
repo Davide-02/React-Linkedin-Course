5a4ba25a93f9c3c1171f6e4f60cc277688ceca5d
Davide Scalone <davidescalone15@gmail.com>
2022-09-13 17:44:55 +0200

    Add Functions and correct files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
-import { App } from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-    </Routes>
-  </BrowserRouter>
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
