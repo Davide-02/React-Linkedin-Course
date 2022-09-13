2d80af8516a490913c08b8e0971bd922ce1718a7
Davide Scalone <davidescalone15@gmail.com>
2022-09-13 11:25:25 +0200

    Adjusting Files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BiArchive } from "react-icons/bi";
 
-function App() {
+export function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+        <BiArchive className="inline-block text-red-400 align-top " />
+        My Website
+      </h1>
     </div>
   );
 }
 
-export default App;
+export function App() {
+  return <Home />;
+}
